310b93efebc0072aea91d9c8d538a42e237a9bf9
nachosanchezsolano <77471637+nachosanchezsolano@users.noreply.github.com>
2024-01-03 23:51:37 -0300

    start commit

---
+++ extraction.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import pathlib
+import re
+import time
+
+def extraction_with_firefox(url,paths,sleep_time):
+
+    """
+    Extraccion datos con firefox utilizando selenium.
+
+    Parámetros:
+    -url: dirección web en la que se quiere comenzar para ejecutar los pasos para alcanzar la descarga del archivo
+    -paths: lista de Xpath que se deben seleccionar para llegar al la descarga deseada en orden específico.
+    -sleep_time:Tiempo de descarga especulada del archivo (en segundos)
+
+    """
+    firefox_options = webdriver.FirefoxOptions()
+    firefox_options.set_preference("browser.download.folderList", 2)
+    firefox_options.set_preference("browser.download.dir",str(pathlib.Path.cwd()))
+    firefox_options.set_preference("--headless",False)
+
+                                
+    driver = webdriver.Firefox(options=firefox_options)
+    driver.get(url)
+
+    try:
+       for path in paths:
+            id=re.search(r"'(.*?)'", path)
+            WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.ID, id.group(1))))
+            
+            click_process= driver.find_element(By.XPATH,path)
+            click_process.click()
+            if path == paths[-1]:
+                time.sleep(sleep_time)
+
+    finally:        
+        driver.quit()
+
+
+url="https://cde.ucr.cjis.gov/LATEST/webapp/#/pages/home"
+
+path=["//a[@id='hr-docs-icon-action']",
+      "//*[@id='dwnnibrs-download-select']/button",
+      "//*[@id='nb-option-71']",
+      "//*[@id='dwnnibrsloc-select']/button",
+      "//*[@id='nb-option-97']",
+      "//*[@id='nibrs-download-button']"]
+
+download_time=3
+
+extraction_with_firefox(url,path,3)
---
+++ main.py
@@ -0,0 +1,24 @@
+from zipfile import ZipFile
+import pandas as pd
+from extraction import extraction_with_firefox
+url="https://cde.ucr.cjis.gov/LATEST/webapp/#/pages/home"
+
+path=["//a[@id='hr-docs-icon-action']",
+      "//*[@id='dwnnibrs-download-select']/button",
+      "//*[@id='nb-option-71']",
+      "//*[@id='dwnnibrsloc-select']/button",
+      "//*[@id='nb-option-97']",
+      "//*[@id='nibrs-download-button']"]
+
+download_time=3
+
+extraction_with_firefox(url,path,3)
+
+with ZipFile('victims.zip', 'r') as file:
+    with file.open("Victims_Age_by_Offense_Category_2022.xlsx") as excel_file:
+        df = pd.read_excel(excel_file)
+
+df = df.iloc[:-1]
+df = df[(df['Victims'] != 'Total') & (df['Victims'] != 'Crimes Against Persons') & (df['Victims'] != 'Crimes Against Property')]
+print(df)
+df.to_csv("Victims_Age_by_Offense_Category_2022.csv", index=False)
